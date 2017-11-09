82c36e2016b80262b969aa66c4cea9f048ea41d4
shun10114@gmail.com <shun10114@gmail.com>
2017-11-09 10:39:28 +0900

    ZIGZAG Company algorithm is added

---
+++ src/main/java/com/algorithm/company/zigzeg/ExtractOddNumbers.java
@@ -0,0 +1,36 @@
+package com.algorithm;
+
+import com.sun.corba.se.impl.io.TypeMismatchException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author HunSeol
+ * @created_date 2017. 11. 8.
+ * @IDE IntelliJ IDEA
+ */
+public class ExtractOddNumbers {
+    public static void main(String[] args) {
+        ExtractOddNumbers extractOddNumbers = new ExtractOddNumbers();
+        System.out.println(extractOddNumbers.extractOddNumbers("ab2v9bc13j5jf4jv21"));
+
+    }
+
+    private int extractOddNumbers(String n) {
+        int result = 0;
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(n);
+        while (m.find()) {
+            try {
+                int value = Integer.parseInt(m.group());
+                if (Integer.parseInt(m.group()) % 2 != 0) {
+                    result += Math.pow(value, 2);
+                }
+            } catch (TypeMismatchException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
